7178c651ee077c1294d68f0275d0ec74aa6d47af
Gabriel Jose S. Esperanza <esperanzagabrieljose@gmail.com>
2024-09-07 19:43:01 +0800

    added function to start,option,about, and quit btn

--- Sample Maze Game/Assets/Script/MainMenu.cs
+++ Sample Maze Game/Assets/Script/MainMenu.cs
@@ -5,8 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void difficulty() //it will go to difficulty tab
     {
         SceneManager.LoadSceneAsync(1);
+    }
+    public void option()//it will go to option tab
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+    public void about()//it will go to about tab
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+    public void quit()//it will exit application
+    {
+        Application.Quit();
     }
 }
