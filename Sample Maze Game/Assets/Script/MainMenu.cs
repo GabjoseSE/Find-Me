26147ad5025ba50e80a33fa6dc444fa9c4f06fe2
Gabriel Jose S. Esperanza <esperanzagabrieljose@gmail.com>
2024-09-16 20:20:08 +0800

    added scenes
    added login page and signup page
    created script for database

--- Sample Maze Game/Assets/Script/MainMenu.cs
+++ Sample Maze Game/Assets/Script/MainMenu.cs
@@ -7,15 +7,15 @@ public class MainMenu : MonoBehaviour
 {
     public void difficulty() //it will go to difficulty tab
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync(3);
     }
     public void option()//it will go to option tab
     {
-        SceneManager.LoadSceneAsync(2);
+        SceneManager.LoadSceneAsync(4);
     }
     public void about()//it will go to about tab
     {
-        SceneManager.LoadSceneAsync(3);
+        SceneManager.LoadSceneAsync(5);
     }
     public void quit()//it will exit application
     {
@@ -23,6 +23,14 @@ public class MainMenu : MonoBehaviour
     }
     public void back()
     {
+        SceneManager.LoadSceneAsync(2);
+    }
+    public void login()
+    {
         SceneManager.LoadSceneAsync(0);
+    }
+    public void signup()
+    {
+        SceneManager.LoadSceneAsync(1);
     }
 }
