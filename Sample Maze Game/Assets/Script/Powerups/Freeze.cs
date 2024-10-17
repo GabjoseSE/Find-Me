607a4235e6516ee69b0dcb2adf6b6f1136e2b1d0
Aerythox <flyerbek.xd0921@gmail.com>
2024-10-17 22:52:47 +0800

    fixed freeze bug

--- Sample Maze Game/Assets/Script/Powerups/Freeze.cs
+++ Sample Maze Game/Assets/Script/Powerups/Freeze.cs
@@ -6,14 +6,21 @@ using UnityEngine.UI;
 
 public class Freeze : MonoBehaviour
 {
+    public ZombieAI zombieBehaviour;
     public NavMeshAgent zombieAgent;
     public float freezeDuration = 10f;
     public Button freezeButton;
+    public float invisDetect = 0f;
+    public float invisAttack = 0f;
     Animator animator;
     public Image cooldownImage;
 
     void Start()
     {
+        if (zombieBehaviour == null)
+        {
+            zombieBehaviour = GameObject.FindWithTag("Zombie").GetComponent<ZombieAI>();
+        }
         cooldownImage.fillAmount = 1;
     }
 
@@ -30,10 +37,16 @@ public class Freeze : MonoBehaviour
     }
     private IEnumerator FreezeZombie(NavMeshAgent zombieAgent)
     {
+        float originalDetect = zombieBehaviour.detectionRange;
+        float originalAttack = zombieBehaviour.attackRange;
+        zombieBehaviour.SetAttackRange(invisAttack);
+        zombieBehaviour.SetDetectionRange(invisDetect);
         zombieAgent.isStopped = true;
         Debug.Log("Zombie frozen!");
 
         yield return new WaitForSeconds(freezeDuration);
+        zombieBehaviour.SetAttackRange(originalAttack);
+        zombieBehaviour.SetDetectionRange(originalDetect);
         zombieAgent.isStopped = false;
         Debug.Log("Zombie unfrozen!");
 
