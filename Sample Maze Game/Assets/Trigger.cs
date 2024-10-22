8caba4f2dae22d21ef4c6adeb9165d1d2cbb478d
Kesh <kresnergaming@gmail.com>
2024-10-22 14:42:13 +0800

    Added Trigger System
    asd

---
+++ Sample Maze Game/Assets/Trigger.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] bool destroyOnTriggerEnter;
+    [SerializeField] string tagFilter;
+    [SerializeField] UnityEvent onTriggerEnter;
+    [SerializeField] UnityEvent onTriggerExit;
+    void OnTriggerEnter(Collider other)
+    {
+        if (!String.IsNullOrEmpty(tagFilter) && !other.gameObject.CompareTag(tagFilter)) return;
+        onTriggerEnter.Invoke();
+        if (destroyOnTriggerEnter)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (!String.IsNullOrEmpty(tagFilter) && !other.gameObject.CompareTag(tagFilter)) return;
+        onTriggerExit.Invoke();
+    }
+}
